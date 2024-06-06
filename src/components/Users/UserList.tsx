f1dc5e459fc6a634a482f9baceb4e06877a21818
DarkFlarex <darkflarexxx@gmail.com>
2024-06-06 21:12:56 +0600

    В UserList добавлена функция для отображения статуса активности пользователя

--- src/components/Users/UserList.tsx
+++ src/components/Users/UserList.tsx
@@ -5,19 +5,22 @@ interface Props{
     user:User;
 }
 
-
 const UserList: React.FC<Props> = ({user}) => {
-
-
+    const showActive = (active: boolean) => {
+        if (active) {
+            return "да";
+        } else {
+            return "нет";
+        }
+    };
 
     return (
         <div className="UserList">
             <span>Имя:{user.name}</span>
             <span>Email:{user.email}</span>
-            <span>Активен: {user.active}</span>
+            <span>Активен: {showActive(user.active)}</span>
             <span>Роль:{user.role}</span>
         </div>
-
     );
 };
 
