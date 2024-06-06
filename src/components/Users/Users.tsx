186195542328c3d0fee6af48a92f12b8fd01f315
DarkFlarex <darkflarexxx@gmail.com>
2024-06-06 22:01:13 +0600

    В Users теперь отображается сообщение, когда список пользователей пуст. Добавлен новый CSS-класс "missing-UserList" для стилизации сообщения.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,17 +2,22 @@ import React from "react";
 import UserList from "./UserList";
 import {User} from "../../type";
 
-
 interface Props {
     users: User[];
 }
-const Users: React.FC <Props> = ({users}) => {
 
+const Users: React.FC <Props> = ({users}) => {
     return (
         <>
-            {users.map(user =>(
+            {users.length === 0 ? (
+                <div className="missing-UserList">
+                    <span>Добавьте пользователя</span>
+                </div>
+            ) : (
+            users.map(user =>(
                 <UserList key={user.id} user={user}/>
-            ))}
+            ))
+            )}
         </>
     );
 };
